dabb0dc27f6236e92f0e4df6c806eaef6d2b0222
Partha Khan <partha92developer@gmail.com>
2023-11-24 11:29:20 +0530

    spa commit

---
+++ JQuery/script_style.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    // scrollTop
+
+    $(".scrolltop").hide("slow");
+    $(window).scroll(function(){
+        var x=$ (document).scrollTop();
+        if(x>50){
+            $(".scrolltop").show("slow");
+        }
+        else{
+            $(".scrolltop").hide("slow");
+        }
+    });
+    $(".scrolltop").click(function(){
+        $(window).scrollTop(0);
+    });
+
+    // nav-menu
+
+  $(".mobile-menu i").click(function(){
+    $(".menu-main").show();
+    $("body").addClass("overflow-hidden")
+  });
+  $(".close-menu i").click(function(){
+    $(".menu-main").hide();
+    $("body").removeClass("overflow-hidden")
+  });
+
+});
